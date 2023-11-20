d6bc6176fb973c34220c5e68f4f7c46ecc0ecf21
Badger <131177569+BrandonPCollins@users.noreply.github.com>
2023-11-20 18:24:00 +0000

    Post Categories

--- myblog/forms.py
+++ myblog/forms.py
@@ -1,18 +1,27 @@
 from django import forms 
-from .models import Post  
+from .models import Post, Category 
 
+#Grab the Categories created in the admin panel 
+choices = Category.objects.all().values_list('name','name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your Title Here!'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Give your post a tag!'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
+#Distinct class for when editing a post
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
