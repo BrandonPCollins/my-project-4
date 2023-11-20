0ff4baf5e8a929d4fa577e646d8fd550ce934a20
Badger <131177569+BrandonPCollins@users.noreply.github.com>
2023-11-20 15:06:13 +0000

    Edit Post Model

--- myblog/forms.py
+++ myblog/forms.py
@@ -12,3 +12,14 @@ class PostForm(forms.ModelForm):
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Add your Title Here!'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Give your post a tag!'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
