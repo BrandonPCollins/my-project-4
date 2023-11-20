f7bed5bc3cfac1b0a9aa3e705c97c16f8b011539
Badger <131177569+BrandonPCollins@users.noreply.github.com>
2023-11-20 17:13:53 +0000

    Login and Registration

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'myblog',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
