706fde66bb6ea970d881905a55ec4fb297dc37b3
Badger <131177569+BrandonPCollins@users.noreply.github.com>
2023-11-21 19:37:50 +0000

    Removing superuser

--- members/forms.py
+++ members/forms.py
@@ -36,13 +36,19 @@ class EditProfileForm(UserChangeForm):
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
-    is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
-    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
-    date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    #is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    #is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    #is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    last_login = forms.DateField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
+        disabled=True
+    )
+    date_joined = forms.DateField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
+        disabled=True
+    )
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 
-                  'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined',)
+                  'last_login', 'date_joined',)
