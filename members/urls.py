60fa3176e20ba8740eaeeb601437a896457ee2a8
Badger <131177569+BrandonPCollins@users.noreply.github.com>
2023-11-21 14:41:13 +0000

    Profile Page Init

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, ShowProfilePageView
 from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
 ]
