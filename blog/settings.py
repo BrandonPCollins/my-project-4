0ac729a271f8284b9f9d16bb5dfccd4dff43cb06
Badger <131177569+BrandonPCollins@users.noreply.github.com>
2023-11-22 18:26:30 +0000

    Debug True

--- blog/settings.py
+++ blog/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'project-4-django-app-dee496c0d081.herokuapp.com',]
 
